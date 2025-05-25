b585761c8976622d4b9a806a2d375eba8a750a28
as03z <SidaniAdnan03@gmail.com>
2025-05-25 18:47:46 +0300

    first commit

---
+++ pedofile/src/App.jsx
@@ -0,0 +1,21 @@
+import Header from './header.jsx'
+import Footer from './footer.jsx'
+import Card from './Card.jsx'
+import './Card.css'
+
+ 
+function App() {
+  
+  return( 
+      <>
+       
+        <Header/>
+         <Card/>
+        <Footer/>
+      </>
+   
+  );
+
+}
+
+export default App
---
+++ pedofile/src/header.jsx
@@ -0,0 +1,20 @@
+
+function Header(){
+return(
+    <header>
+        <h1>PDF</h1>
+        <nav>
+            <u1>
+                <li><a href="#">home</a></li>
+                <li><a href="#">about</a></li>
+                <li><a href="#">services</a></li>
+                <li><a href="#">contact</a></li>
+            </u1>
+        </nav>
+        <hr></hr>
+    </header>
+)
+
+}
+
+export default Header 
