83ef8a90857bc57ef75256938a3774b024d2cee1
MardiniO <mardiniomar3@gmail.com>
2025-05-26 22:16:35 +0300

    Actually changed project name.

--- PDFconfigurator/src/App.jsx
+++ PDFconfigurator/src/App.jsx
@@ -1,21 +1,16 @@
-import Header from './header.jsx'
-import Footer from './footer.jsx'
-import Card from './Card.jsx'
-import './Card.css'
+import Header from "./header.jsx";
+import Footer from "./footer.jsx";
+import Card from "./Card.jsx";
+import "./Card.css";
 
- 
 function App() {
-  
-  return( 
-      <>
-       
-        <Header/>
-         <Card/>
-        <Footer/>
-      </>
-   
+  return (
+    <>
+      <Header />
+      <Card />
+      <Footer />
+    </>
   );
-
 }
 
-export default App
+export default App;
--- PDFconfigurator/src/Card.jsx
+++ PDFconfigurator/src/Card.jsx
@@ -1,14 +1,14 @@
-import wordpic from './assets/word.jpg'
+import wordpic from "./assets/word.jpg";
 
-function Card(){
-        return(
-            <div className="card">
-                <img src={wordpic} alt="word document picture"></img>
-                <h2>Word Documnet</h2>
-                <p>Convert pdf file from and to word documents</p>
-                <a href="cardPage">Learn More</a>
-            </div>
-        );
+function Card() {
+  return (
+    <div className="card">
+      <img src={wordpic} alt="word document picture"></img>
+      <h2>Word Documnet</h2>
+      <p>Convert pdf file from and to word documents</p>
+      <a href="cardPage">Learn More</a>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
